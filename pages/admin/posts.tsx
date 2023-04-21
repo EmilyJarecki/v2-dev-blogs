0e9683e55a811017270772730a7e057e3b88c19e
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-21 10:12:35 -0500

    completed section 9- admin nav

--- pages/admin/posts.tsx
+++ pages/admin/posts.tsx
@@ -1,18 +1,17 @@
+import AdminLayout from '@/components/layout/AdminLayout';
 import { NextPage } from 'next';
-import {AiOutlineDashboard, AiOutlineContainer, AiOutlineTeam, AiOutlineMail, AiOutlineContacts} from 'react-icons/ai'
 
 interface Props {}
 
-const navItems = [
-    {href: "/admin", icon: AiOutlineDashboard, label: "Dashboard"},
-    {href: "/admin/posts", icon: AiOutlineContainer, label: "Posts"},
-    {href: "/admin/users", icon: AiOutlineTeam, label: "Users"},
-    {href: "/admin/comments", icon: AiOutlineMail, label: "Comments"},
-    {href: "/admin/contacts", icon: AiOutlineContacts, label: "Contacts"},
-]
+
 
 const Posts: NextPage<Props> = () => {
-  return <div>posts</div>;
+  return (
+  
+  <AdminLayout>
+    posts
+  </AdminLayout>
+    )
 };
 
 export default Posts;
