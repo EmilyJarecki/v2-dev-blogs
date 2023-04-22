9dc8e679d99eddfac57843e493e8131db9066647
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-22 15:02:08 -0500

    styling editor

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -3,15 +3,26 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import ToolBar from './ToolBar';
 import Underline from '@tiptap/extension-underline';
+import Placeholder from '@tiptap/extension-placeholder';
 
 
 interface Props { }
 
 const Editor: FC<Props> = (props): JSX.Element => {
-    const editor = useEditor({ extensions: [StarterKit, Underline] });
+    const editor = useEditor({ 
+        extensions: [StarterKit, Underline, Placeholder.configure({
+            placeholder: 'Type something',
+        })], 
+        editorProps:{
+            attributes:{
+                class: 'prose-lg prose focus:outline-none dark:prose-invert max-w-full mx-auto h-full'
+            }
+        }
+    });
     return (
-        <div>
-                <ToolBar editor={editor} />
+        <div className="p-3 dark:bg-primary-dark bg-primary transition">
+            <ToolBar editor={editor} />
+            <div className="h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3"/>
             <EditorContent editor={editor} />
         </div>
     )
