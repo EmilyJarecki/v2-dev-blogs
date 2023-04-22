ff5cffe90e1a25513f3da50bae469c1f553200f9
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-22 15:38:16 -0500

    show which buttons are active in editor

--- components/editor/ToolBar/index.tsx
+++ components/editor/ToolBar/index.tsx
@@ -67,37 +67,37 @@ const ToolBar: FC<Props> = ({ editor }): JSX.Element | null => {
             <DropdownOptions options={options} head={<Head />} />
             <div className="h-4 w-[1px] bg-secondary-dark dark:bg-secondary-light mx-8" />
             <div className="flex items-center space-x-3">
-                <Button onClick={()=> getFocusedEditor(editor).toggleBold().run()}>
+                <Button active={editor.isActive("bold")} onClick={()=> getFocusedEditor(editor).toggleBold().run()}>
                     <BsTypeBold />
                 </Button>
-                <Button onClick={()=> getFocusedEditor(editor).toggleItalic().run()}>
+                <Button active={editor.isActive("italic")} onClick={()=> getFocusedEditor(editor).toggleItalic().run()}>
                     <BsTypeItalic />
                 </Button>
-                <Button onClick={()=> getFocusedEditor(editor).toggleUnderline().run()}>
+                <Button active={editor.isActive("underline")} onClick={()=> getFocusedEditor(editor).toggleUnderline().run()}>
                     <BsTypeUnderline />
                 </Button>
-                <Button onClick={()=> getFocusedEditor(editor).toggleStrike().run()}>
+                <Button active={editor.isActive("strike")} onClick={()=> getFocusedEditor(editor).toggleStrike().run()}>
                     <BsTypeStrikethrough />
                 </Button>
             </div>
             <div className="h-4 w-[1px] bg-secondary-dark dark:bg-secondary-light mx-8" />
             <div className="flex items-center space-x-3">
-                <Button onClick={()=> getFocusedEditor(editor).toggleBlockquote().run()}>
+                <Button active={editor.isActive("blockquote")} onClick={()=> getFocusedEditor(editor).toggleBlockquote().run()}>
                     <RiDoubleQuotesL />
                 </Button>
-                <Button onClick={()=> getFocusedEditor(editor).toggleCode().run()}>
+                <Button active={editor.isActive("code")} onClick={()=> getFocusedEditor(editor).toggleCode().run()}>
                     <BsCode />
                 </Button>
-                <Button onClick={()=> getFocusedEditor(editor).toggleCodeBlock().run()}>
+                <Button active={editor.isActive("codeBlock")} onClick={()=> getFocusedEditor(editor).toggleCodeBlock().run()}>
                     <BsBraces />
                 </Button>
                 <Button>
                     <BsLink45Deg />
                 </Button>
-                <Button onClick={()=> getFocusedEditor(editor).toggleOrderedList().run()}>
+                <Button active={editor.isActive("orderedList")} onClick={()=> getFocusedEditor(editor).toggleOrderedList().run()}>
                     <BsListOl />
                 </Button>
-                <Button onClick={()=> getFocusedEditor(editor).toggleBulletList().run()}>
+                <Button active={editor.isActive("bulletList")} onClick={()=> getFocusedEditor(editor).toggleBulletList().run()}>
                     <BsListUl />
                 </Button>
             </div>
