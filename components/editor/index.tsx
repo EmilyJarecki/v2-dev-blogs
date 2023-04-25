fbc7f9b98abff4666a0d222e474e1eee756990a2
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-25 13:36:06 -0500

    image upload working

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, ChangeEventHandler } from 'react';
 import { useEditor, EditorContent, getMarkRange, Range, getMarksBetween } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import ToolBar from './ToolBar';
@@ -7,16 +7,38 @@ import Placeholder from '@tiptap/extension-placeholder';
 import Link from '@tiptap/extension-link';
 import Youtube from '@tiptap/extension-youtube';
 import EditLink from './Link/EditLink';
-import GalleryModal from './GalleryModal';
+import GalleryModal, { ImageSelectionResult } from './GalleryModal';
+import TipTapImage from "@tiptap/extension-image"
+import axios from 'axios';
 
 interface Props { }
 
 const Editor: FC<Props> = (props): JSX.Element => {
     const [selectionRange, setSelectionRange] = useState<Range>()
     const [showGallery, setShowGallery] = useState(false)
+    const [uploading, setUploading] = useState(false)
+    const [images, setImages] = useState<{ src: string }[]>([])
+
+    const fetchImages = async () => {
+        const { data } = await axios("/api/image")
+        setImages(data.images)
+    }
+    const handleImageUpload = async (image: File) => {
+        setUploading(true)
+        const formData = new FormData()
+        //this name matters- it's the same one as in index.ts
+        formData.append("image", image)
+        const { data } = await axios.post('/api/image', formData)
+        setUploading(false)
+        setImages([data, ...images])
+
+        console.log(data)
+    }
+
 
     const editor = useEditor({
-        extensions: [StarterKit,
+        extensions: [
+            StarterKit,
             Underline,
             Link.configure({
                 autolink: false,
@@ -34,13 +56,20 @@ const Editor: FC<Props> = (props): JSX.Element => {
                 HTMLAttributes: {
                     class: "mx-auto rounded"
                 }
+            }),
+            TipTapImage.configure({
+                HTMLAttributes: {
+                    class: 'mx-auto'
+                }
             })
 
         ],
         editorProps: {
             handleClick(view, pos, event) {
                 const { state } = view
-                const selectionRange = getMarkRange(state.doc.resolve(pos), state.schema.marks.link)
+                const selectionRange = getMarkRange(
+                    state.doc.resolve(pos),
+                    state.schema.marks.link)
                 if (selectionRange) setSelectionRange(selectionRange)
             },
             attributes: {
@@ -49,24 +78,45 @@ const Editor: FC<Props> = (props): JSX.Element => {
         }
     });
 
+    const handleImageSelection = (result: ImageSelectionResult) => {
+        editor
+            ?.chain()
+            .focus()
+            .setImage({ src: result.src, alt: result.altText })
+            .run();
+    }
+
     useEffect(() => {
         if (editor && selectionRange) {
             editor.commands.setTextSelection(selectionRange)
         }
     }, [editor, selectionRange])
 
+    useEffect(() => {
+        fetchImages()
+    }, [])
 
     return (
         <>
             <div className="p-3 dark:bg-primary-dark bg-primary transition">
-                <ToolBar editor={editor} onOpenImageClick={()=> setShowGallery(true)}/>
+                <input 
+                type="text" 
+                className="py-2 outline-none bg-transparent w-full border-0 border-b-[1px] border-secondary-dark dark:border-secondary-light text-3xl font-semibold italic text-primary-dark dark:text-primary-light mb-3" 
+                placeholder="Title"/>
+
+                <ToolBar editor={editor} onOpenImageClick={() => setShowGallery(true)} />
                 <div className="h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3" />
                 {editor ? <EditLink editor={editor} /> : null}
                 <EditorContent editor={editor} />
             </div>
-            <GalleryModal 
-            visible={showGallery} 
-            onClose={() => setShowGallery(false)}/>
+            <GalleryModal
+                visible={showGallery}
+                onClose={() => setShowGallery(false)}
+                onSelect={handleImageSelection}
+                images={images}
+                onFileSelect={handleImageUpload}
+                uploading={uploading}
+            />
         </>
 
     )
