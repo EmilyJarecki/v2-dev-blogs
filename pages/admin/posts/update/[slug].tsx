528ee3c73424167a83416a05fc9e29d0a59ee2e7
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-27 22:44:57 -0500

    able to view post via slug in url for updating

--- pages/admin/posts/update/[slug].tsx
+++ pages/admin/posts/update/[slug].tsx
@@ -1,9 +1,53 @@
+import Editor, { FinalPost } from '@/components/editor';
+import AdminLayout from '@/components/layout/AdminLayout';
+import Post from '@/models/Posts';
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next';
 import { FC } from 'react';
 
-interface Props {}
+interface PostResponse extends FinalPost {
+    id: string;
+}
 
-const Update: FC<Props> = (props): JSX.Element => {
-  return <div>Update</div>;
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>
+
+const Update: NextPage<Props> = ({post}) => {
+    return (
+    <AdminLayout title="Update">
+        <div className="max-w-4xl mx-auto">
+        <Editor initialValue={post} onSubmit={()=>{}} btnTitle="Update"/>
+
+
+        </div>
+    </AdminLayout>
+    )
 };
+
+
+interface ServerSideResponse {
+    post: PostResponse
+}
+
+export const getServerSideProps: GetServerSideProps<ServerSideResponse> = async (context) => {
+    const slug = context.query.slug as string
+
+    const post = await Post.findOne({ slug })
+    if (!post) return { notFound: true }
+
+    const { _id, meta, title, content, thumbnail, tags } = post
+
+    return {
+        props: {
+            post: {
+                id: _id.toString(),
+                title, 
+                content, 
+                tags: tags.join(", "), 
+                thumbnail: thumbnail?.url || "",
+                slug,
+                meta
+            }
+        }
+    }
+}
 
 export default Update;
