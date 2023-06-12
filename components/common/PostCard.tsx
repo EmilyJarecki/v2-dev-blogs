d65878379fdb3d68b79a885555723f2479c441ac
EmilyJarecki <jareckiemily@yahoo.com>
2023-06-12 15:55:33 -0500

    post cards complete

--- components/common/PostCard.tsx
+++ components/common/PostCard.tsx
@@ -3,9 +3,12 @@ import { PostDetail } from '@/utils/types';
 import { dividerClasses } from '@mui/material';
 import Image from 'next/image';
 import dateFormat from 'dateformat';
+import Link from 'next/link';
 
 interface Props {
-  post: PostDetail
+  post: PostDetail;
+  busy?: boolean;
+  onDeleteClick?(): void
 }
 
 const trimText = (text: string, trimBy: number) => {
@@ -13,7 +16,7 @@ const trimText = (text: string, trimBy: number) => {
   return text.substring(0, trimBy).trim() + "..."
 }
 
-const PostCard: FC<Props> = ({ post }): JSX.Element => {
+const PostCard: FC<Props> = ({ post, busy, onDeleteClick }): JSX.Element => {
   //destructuring content
   const { title, slug, meta, createdAt, tags, thumbnail } = post
   return <div className="rounded shadow-sm shadow-secondary-dark overflow-hidden bg-primary dark:bg-primary-dark transition flex flex-col h-full">
@@ -24,25 +27,45 @@ const PostCard: FC<Props> = ({ post }): JSX.Element => {
         //Image is a part of Next
         (<Image src={thumbnail} layout="fill" alt="Thumbnail" />)}
     </div>
+
     {/* post info */}
     <div className="p-2 flex-1 flex flex-col">
-      <div className="flex items-center justify-between text-sm text-primary-dark dark:text-primary">
-        <div className="flex item-center space-x-1">
-          {tags.map((t, index) => (
-            <span key={t + index}>#{t}</span>
-          ))}
+      <Link href={"/" + slug}>
+        <div className="flex items-center justify-between text-sm text-primary-dark dark:text-primary">
+          <div className="flex item-center space-x-1">
+            {tags.map((t, index) => (
+              <span key={t + index}>#{t}</span>
+            ))}
+          </div>
+          {/* installed a npm package, dateformat  */}
+          <span>{dateFormat(createdAt, "d-mmm-yyyy")}</span>
+
+
         </div>
-        {/* installed a npm package, dateformat  */}
-        <span>{dateFormat(createdAt, "d-mmm-yyyy")}</span>
-
-
-      </div>
-      <h1 className="font-semibold text-primary-dark dark:text-primary">{trimText(title, 50)}</h1>
-      <p className="text-secondary-dark">{trimText(meta, 70)}</p>
-
+        <h1 className="font-semibold text-primary-dark dark:text-primary">{trimText(title, 50)}</h1>
+        <p className="text-secondary-dark">{trimText(meta, 70)}</p>
+      </Link>
       <div className="flex justify-end items-center h-8 mt-auto space-x-4 text-primary-dark dark:text-primary">
-        <button className="hover:underline">Edit</button>
-        <button className="hover:underline">Delete</button>
+
+        {busy ? 
+        (
+        <span className="animate-pulse">Removing</span>
+        ) 
+        : 
+        (
+          <>
+            <Link href={"/admin/posts/update/" + slug}>
+              <p className="hover:underline">Edit</p>
+            </Link>
+            <button 
+            onClick={onDeleteClick} 
+            className="hover:underline">
+              Delete
+              </button>
+          </>
+        )}
+
+
       </div>
     </div>
   </div>
--- pages/admin/posts/index.tsx
+++ pages/admin/posts/index.tsx
@@ -2,7 +2,8 @@ import AdminLayout from '@/components/layout/AdminLayout';
 import { NextPage } from 'next';
 import { useState } from 'react';
 import PostCard from '@/components/common/PostCard';
-interface Props {}
+import Link from 'next/link';
+interface Props { }
 
 const posts = [
   {
@@ -11,7 +12,7 @@ const posts = [
     meta: "This is my first post, and typesetting industry. Lorem Ipsum has been the industry's standard",
     tags: ["post"],
     thumbnail:
-    "https://compote.slate.com/images/5294e6d0-53ed-4a4a-a350-7eaeab72ac93.jpeg?crop=1560%2C1040%2Cx0%2Cy0",
+      "https://compote.slate.com/images/5294e6d0-53ed-4a4a-a350-7eaeab72ac93.jpeg?crop=1560%2C1040%2Cx0%2Cy0",
     createdAt: "Sat Jun 09 2007 17:46:21"
   },
   {
@@ -20,25 +21,28 @@ const posts = [
     meta: "This is my second post, and typesetting industry. Lorem Ipsum has been the industry's standard",
     tags: ["post"],
     thumbnail:
-    "https://cdn.britannica.com/59/94459-050-DBA42467/Skyline-Chicago.jpg",
+      "https://cdn.britannica.com/59/94459-050-DBA42467/Skyline-Chicago.jpg",
     createdAt: "Sat Jun 09 2007 17:46:21"
   }
 ]
 
 const Posts: NextPage<Props> = () => {
-const [postsToRender, setPostsToRender] = useState(posts)
+  const [postsToRender, setPostsToRender] = useState(posts)
 
   return (
-  
-  <AdminLayout>
-    <div className="max-w-4xl mx-auto p-3">
-      <div className="grid grid-cols-3 gap-4">
-        {postsToRender.map((post, index) => 
-        (<PostCard key = {index} post={post}/>))}
+
+    <AdminLayout>
+      <div className="max-w-4xl mx-auto p-3">
+        <div className="grid grid-cols-3 gap-4">
+          {postsToRender.map((post, index) =>
+          (
+              <PostCard key={post.slug} post={post} />
+
+          ))}
+        </div>
       </div>
-    </div>
-  </AdminLayout>
-    )
+    </AdminLayout>
+  )
 };
 
 export default Posts;
